48d4803f4a60eb4ff3a85d1932041fa72c7378bc
mpk72 <matthew.kelly2@gmail.com>
2014-01-10 15:25:04 -0500

    Wrote a script to plot work vs speed

--- GAIT_Walk_Power/MAIN__work_vs_speed.m
+++ GAIT_Walk_Power/MAIN__work_vs_speed.m
@@ -14,13 +14,13 @@ INPUT.physical.coeff_friction = 0.8;
 %%%% Constraints %%%%
 INPUT.constraint.duration_single_stance = [0.1; 1.2];
 INPUT.constraint.duration_double_stance = [0.02; 0.8];
-INPUT.constraint.speed = [0.1; 0.2];
+INPUT.constraint.speed = [0.2; 0.2];
 INPUT.constraint.step_distance = 0.4;
 
 %%%% Optimization %%%%
-INPUT.optimize.solver = 'ipopt';   %{'ipopt', 'snopt'}
+INPUT.optimize.solver = 'snopt';   %{'ipopt', 'snopt'}
 INPUT.optimize.tolerance = 1e-3;
-INPUT.optimize.max_mesh_iter = 2;
+INPUT.optimize.max_mesh_iter = 3;
 
 %%%% Cost Function %%%%
 INPUT.cost.actuator_weight = 1e-3;
@@ -28,13 +28,41 @@ INPUT.cost.actuator_rate_weight = 1e-3;
 INPUT.cost.method = 'Work';
 
 %%%% Input / Output parameters %%%%
-INPUT.io.loadPrevSoln = false;
-INPUT.io.saveSolution = false;
-INPUT.io.createPlots = true;
+INPUT.io.loadPrevSoln = true;
+INPUT.io.saveSolution = true;
+INPUT.io.createPlots = false;
 INPUT.io.runAnimation = false;
 
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
 %                      Run Trajectory Optimization:                       %
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
 
-[OUTPUT, plotInfo] = Trajectory_Walk(INPUT);
+N_data_points = 25;
+min_speed = 0.2;
+max_speed = 2.0;
+
+List_Speeds = linspace(min_speed,max_speed,N_data_points);
+List_Work = zeros(size(List_Speeds));
+List_exit_code = zeros(size(List_Speeds));
+
+for i=1:N_data_points
+    INPUT.constraint.speed = List_Speeds(i)*[1;1];
+    if i==1
+        INPUT.io.loadPrevSoln = false;
+    else
+        INPUT.io.loadPrevSoln = true;
+    end    
+    OUTPUT = Trajectory_Walk(INPUT);
+    List_Work(i) = OUTPUT.result.objective;
+    List_exit_code(i) = OUTPUT.result.nlpinfo;
+end
+
+plot(List_Speeds,List_Work)
+title('Work vs Speed')
+xlabel('Speed (m/s)')
+ylabel('Work (J)')
+
+
+
+
+
