a9fc6badfec2943211b64ed981eedcb501c21b17
Matthew Kelly <matthew.kelly2@gmail.com>
2014-01-16 11:07:42 -0500

    further improvements to plotting structure

--- shared/formatData.m
+++ shared/formatData.m
@@ -104,6 +104,52 @@ switch phase
         
         Data.energy = Energy;
         
+    case 'F'
+        [Position, Velocity, Power, Energy] = kinematics_flight(States, Actuators, Parameters);
+        
+        Data.state.th1 = States(:,3); % (rad) Leg One absolute angle
+        Data.state.th2 = States(:,4); % (rad) Leg Two absolute angle
+        Data.state.L1 = States(:,5); % (m) Leg One length
+        Data.state.L2 = States(:,6); % (m) Leg Two length
+        Data.state.dth1 = States(:,9); % (rad/s) Leg One absolute angular rate
+        Data.state.dth2 = States(:,10); % (rad/s) Leg Two absolute angular rate
+        Data.state.dL1 = States(:,11); % (m/s) Leg One extension rate
+        Data.state.dL2 = States(:,12); % (m/s) Leg Two extensioin rate
+        Data.state.x1 = States(:,1); % (m) Foot One Horizontal Position
+        Data.state.y1 = States(:,2); % (m) Foot One Vertical Position
+        Data.state.x2 = Position.footTwo.x; % (m) Foot Two Horizontal Position
+        Data.state.y2 = Position.footTwo.y; % (m) Foot Two Vertical Position
+        Data.state.x0 = Position.hip.x; % (m) Hip Horizontal Position
+        Data.state.y0 = Position.hip.y; % (m) Hip Vertical Position
+        Data.state.dx1 = States(:,7); % (m) Foot One Horizontal Velocity
+        Data.state.dy1 = States(:,8); % (m) Foot One Vertical Velocity
+        Data.state.dx2 = Velocity.footTwo.x; % (m) Foot Two Horizontal Velocity
+        Data.state.dy2 = Velocity.footTwo.y; % (m) Foot Two Vertical Velocity
+        Data.state.dx0 = Velocity.hip.x; % (m) Hip Horizontal Velocity
+        Data.state.dy0 = Velocity.hip.y; % (m) Hip Vertical Velocity
+        
+        Data.control.F1 = Actuators(:,1); % (N) Compresive axial force in Leg One
+        Data.control.F2 = Actuators(:,2); % (N) Compresive axial force in Leg Two
+        Data.control.T1 = zeros(n,1); % (Nm) External torque applied to Leg One
+        Data.control.Thip = Actuators(:,3); % (Nm) Hip torque applied to Leg Two from Leg One
+        Data.control.T2 = zeros(n,1); % (Nm) External torque applied to Leg Two
+        
+        Data.contact.H1 = zeros(n,1); %(N) Foot One, horizontal contact force
+        Data.contact.V1 = zeros(n,1); %(N) Foot One, vertical contact force
+        Data.contact.H2 = zeros(n,1); %(N) Foot Two, horizontal contact force
+        Data.contact.V2 = zeros(n,1); %(N) Foot Two, vertical contact force
+        
+        Data.contact.Mag1 = zeros(n,1);              %(N) Foot One, contact force magnitude
+        Data.contact.Ang1 = zeros(n,1);      %(rad) Foot One, contact force angle from vertical
+        Data.contact.Mag2 = zeros(n,1); %(N) Foot Two, contact force magnitude
+        Data.contact.Ang2 = zeros(n,1); %(rad) Foot Two, contact force angle from vertical
+        
+        Data.power = Power;
+        Data.power.ankleOne = zeros(n,1); %(W) Ankle One, power consumption
+        Data.power.ankleTwo = zeros(n,1); %(W) Ankle Two, power consumption
+        
+        Data.energy = Energy;
+        
     otherwise
         error('Unsupported Phase')
 end
--- shared/getPlotInfo.m
+++ shared/getPlotInfo.m
@@ -52,9 +52,6 @@ for iphase = 1:length(P.phase)
             
         case 'D' %Double stance, with Stance_Foot at origin
             
-            D(iphase).time = time;
-            D(iphase).phase = phase;
-            
             IdxState = 1:4;
             IdxAct = 5:6;
             IdxActRate = 1:2;
@@ -73,7 +70,7 @@ for iphase = 1:length(P.phase)
             [Parameters.y2, n2] = feval(P.ground.func,-Step_Dist,[]);
             [~, n1] = feval(P.ground.func,0,[]);
             
-             Data = formatData(time, phase, States, Actuators, Parameters);
+            Data = formatData(time, phase, States, Actuators, Parameters);
             names = fieldnames(Data);
             for i=1:length(names)
                 D(iphase).(names{i}) = Data.(names{i});
@@ -95,11 +92,6 @@ for iphase = 1:length(P.phase)
             D(iphase).step.distance = Step_Dist;
             
         case 'F' %Single stance, with Stance_Foot == Foot_One
-                        
-            error('NEED TO MOVE THIS TO formatData.m');
-            
-            D(iphase).time = time;
-            D(iphase).phase = phase;
             
             IdxState = 1:12;
             IdxAct = 13:15;
@@ -114,48 +106,16 @@ for iphase = 1:length(P.phase)
             AbsNeg = output.result.interpsolution.phase(iphase).control(:,IdxAbsNeg);
             Parameters = P.dynamics;
             
+             Data = formatData(time, phase, States, Actuators, Parameters);
+            names = fieldnames(Data);
+            for i=1:length(names)
+                D(iphase).(names{i}) = Data.(names{i});
+            end
+            
             Step_Dist = output.result.solution.parameter;
-            
-            D(iphase).state.th1 = States(:,3); % (rad) Leg One absolute angle
-            D(iphase).state.th2 = States(:,4); % (rad) Leg Two absolute angle
-            D(iphase).state.L1 = States(:,5); % (m) Leg One length
-            D(iphase).state.L2 = States(:,6); % (m) Leg Two length
-            D(iphase).state.dth1 = States(:,9); % (rad/s) Leg One absolute angular rate
-            D(iphase).state.dth2 = States(:,10); % (rad/s) Leg Two absolute angular rate
-            D(iphase).state.dL1 = States(:,11); % (m/s) Leg One extension rate
-            D(iphase).state.dL2 = States(:,12); % (m/s) Leg Two extensioin rate
-            D(iphase).state.x = States(:,1); % (m) Foot One Horizontal Position
-            D(iphase).state.y = States(:,2); % (m) Foot One Vertical Position
-            D(iphase).state.dx = States(:,7); % (m) Foot One Horizontal Velocity
-            D(iphase).state.dy = States(:,8); % (m) Foot One Vertical Velocity
-            
-            D(iphase).control.F1 = Actuators(:,1); % (N) Compresive axial force in Leg One
-            D(iphase).control.F2 = Actuators(:,2); % (N) Compresive axial force in Leg Two
-            D(iphase).control.T1 = zeros(nTime,1); % (Nm) External torque applied to Leg One
-            D(iphase).control.Thip = Actuators(:,3); % (Nm) Hip torque applied to Leg Two from Leg One
-            D(iphase).control.T2 = zeros(nTime,1); % (Nm) External torque applied to Leg Two
-            
-            D(iphase).contact.H1 = zeros(nTime,1); %(N) Foot One, horizontal contact force
-            D(iphase).contact.V1 = zeros(nTime,1); %(N) Foot One, vertical contact force
-            D(iphase).contact.H2 = zeros(nTime,1); %(N) Foot Two, horizontal contact force
-            D(iphase).contact.V2 = zeros(nTime,1); %(N) Foot Two, vertical contact force
-            
-            D(iphase).contact.Mag1 = zeros(nTime,1);              %(N) Foot One, contact force magnitude
-            D(iphase).contact.Ang1 = zeros(nTime,1);      %(rad) Foot One, contact force angle from vertical
-            D(iphase).contact.Mag2 = zeros(nTime,1); %(N) Foot Two, contact force magnitude
-            D(iphase).contact.Ang2 = zeros(nTime,1); %(rad) Foot Two, contact force angle from vertical
+           
             D(iphase).contact.Bnd1 = zeros(2,1);
             D(iphase).contact.Bnd2 = zeros(2,1);
-            
-            [Position, Velocity, Power, Energy] = kinematics_flight(States, Actuators, Parameters);
-            D(iphase).position = Position;
-            D(iphase).velocity = Velocity;
-            
-            D(iphase).power = Power;
-            D(iphase).power.ankleOne = zeros(nTime,1); %(W) Ankle One, power consumption
-            D(iphase).power.ankleTwo = zeros(nTime,1); %(W) Ankle Two, power consumption
-            
-            D(iphase).energy = Energy;
             
             [cost, ~, info] = costFunc(States, Actuators, Actuator_Rate,...
                 P.dynamics,P.cost , AbsPos, AbsNeg, D(iphase).phase, Step_Dist);
